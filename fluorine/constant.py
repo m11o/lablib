78b2429ee5ce2f8037bf2fb6031bfdea78d032e7
masahito1997 <velonica.gale2227@gmail.com>
2021-05-28 21:31:40 +0900

    add constant list file

---
+++ fluorine/constant.py
@@ -0,0 +1,8 @@
+ANIMAL_NUMBERS = [
+    'A', 'B', 'C', 'G', 'H', 'I', 'K', 'L', 'N', 'Q'
+]
+
+CONTEXTS = [
+    'HC1', 'A1preES', 'A1postES', 'HC2', 'HC3', 'A2_1', 'A2_2',
+    'A3_1', 'A3_2', 'B1', 'HC4', 'A4preES', 'A4postES', 'HC5', 'HC6', 'A5', 'B2'
+]

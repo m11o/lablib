db9460b70347d9eb6cb1487f0c23ad31e004c6e7
masahito1997 <velonica.gale2227@gmail.com>
2021-05-30 12:43:30 +0900

    implement method to create table

---
+++ utils/databases/fluorine/create_tables.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Table, Column, Integer, String, Boolean, Float, create_engine, MetaData, DECIMAL, DATETIME
+
+url = 'mysql+pymysql://root:@172.20.0.2:3306/lablib_db?charset=utf8'
+engine = create_engine(url)
+meta = MetaData(engine)
+
+Table(
+    'experiments', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), index=True, unique=True)
+)
+Table(
+    'animals', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('experiment_id', Integer, nullable=False, index=True),
+    Column('identification', String(255), nullable=False, index=True, unique=True)
+)
+Table(
+    'contexts', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('experiment_id', Integer, nullable=False, index=True),
+    Column('name', String(255), nullable=False),
+    Column('sort_index', Integer, default=0)
+)
+Table(
+    'cells', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('animal_id', Integer, nullable=False, index=True),
+    Column('context_id', Integer, nullable=False, index=True),
+    Column('name', String(255), nullable=False),
+    Column('engram', Boolean, default=False),
+    Column('engram_id', Integer, nullable=True, default=None)
+)
+Table(
+    'fluorescences', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('cell_id', Integer, nullable=False, index=True),
+    Column('value', Float, nullable=False),
+    Column('diff_time', Float, nullable=False),
+    Column('time', Float, nullable=False)
+)
+
+meta.create_all()
